9cd1b7f28ab085f1056f5440bcaf986339cb2990
vcnaotem-boop <vcnaotem@gmail.com>
2026-07-27 14:02:14 -0400

    main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Tracker from "./Tracker.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Tracker />
+  </React.StrictMode>
+);
